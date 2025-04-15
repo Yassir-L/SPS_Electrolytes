09af650ac278841fb69f8cea022c6f1053b38d84
Yassir-L <37481089+Yassir-L@users.noreply.github.com>
2025-04-15 20:21:21 +0100

    Update data_loader.py
    More general functions for data loader

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import os
 
-def get_excel_path():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(base_dir, "..", "data", "LiPF6_data.xlsx")
+DEFAULT_FILE = "LiPF6_data.xlsx"
 
-def load_data(sheet_name):
-    excel_path = get_excel_path()
+def get_excel_path(file_name=DEFAULT_FILE):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(base_dir, "..", "data", file_name)
+
+def load_data(sheet_name, file_name=DEFAULT_FILE):
+    excel_path = get_excel_path(file_name)
     return pd.read_excel(excel_path, sheet_name=sheet_name, engine="openpyxl")
 
-def save_data(df, sheet_name):
-    excel_path = get_excel_path()
-    # Load existing sheets
+def save_data(df, sheet_name, file_name=DEFAULT_FILE):
+    excel_path = get_excel_path(file_name)
     with pd.ExcelWriter(excel_path, mode='a', engine='openpyxl', if_sheet_exists="replace") as writer:
         df.to_excel(writer, sheet_name=sheet_name, index=False)
-
