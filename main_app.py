fcc08d742cf210531f8260338e556f5ab0f7d882
Yassir-L <37481089+Yassir-L@users.noreply.github.com>
2025-06-17 14:33:22 +0100

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,30 +5,37 @@ from modules.references_module import show as show_references
 from modules.analytics_module import show as show_analytics
 from modules.market_intelligence_module import show as show_market_intel
 from modules.ideas_module import show as show_ideas
+from modules.ai_single_question import show as show_ai_simple
+from modules.avicenne_module import show as show_avicenne
+
 
 st.set_page_config(layout="wide")
 
 st.sidebar.title("🔍 Navigation")
 page = st.sidebar.radio("Go to:", [
+    "Ai Support System",
+    "Avicenne Insights",
     "Map",
     "Database Manager",
     "References",
-    "Analytics",
+    "Self-Assessment",
     "Market Intelligence",
     "💡 Suggestions"
 ])
 
-if page == "Map":
+if page == "Ai Support System":
+    show_ai_simple()
+elif page == "Map":
     show_map()
+elif page == "Avicenne Insights":
+    show_avicenne()
 elif page == "Database Manager":
     show_data_manager()
 elif page == "References":
     show_references()
-elif page == "Analytics":
+elif page == "Self-Assessment":
     show_analytics()
 elif page == "Market Intelligence":
     show_market_intel()
 elif page == "💡 Suggestions":
     show_ideas()
-
-st.sidebar.markdown("🆕 Version: Apr 16 Update")
