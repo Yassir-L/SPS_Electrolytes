e5f782f11e8b69870e0008e71239d72bf405f1ff
Yassir-L <37481089+Yassir-L@users.noreply.github.com>
2025-06-18 12:43:40 +0100

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -8,6 +8,7 @@ from modules.ideas_module import show as show_ideas
 from modules.ai_single_question import show as show_ai_simple
 from modules.avicenne_module import show as show_avicenne
 from modules.home_module import show as show_home
+from modules.LiFSI_data import show as show_raw_materials
 
 
 st.set_page_config(layout="wide")
@@ -15,7 +16,8 @@ st.set_page_config(layout="wide")
 st.sidebar.title("🔍 Navigation")
 page = st.sidebar.radio("Go to:", [
     "🏠 Accueil", 
-    "Map",  
+    "Map",
+    "LiFSI",
     "Avis d'experts", 
     "Self-Assessment",
     "Ai Support System",
@@ -29,6 +31,8 @@ if page == "🏠 Accueil":
     show_home()
 elif page == "Map":
     show_map()
+elif page == "LiFSI":
+    show_raw_materials()
 elif page == "Ai Support System":
     show_ai_simple()
 elif page == "Avis d'experts":  # nouveau nom utilisé ici aussi
